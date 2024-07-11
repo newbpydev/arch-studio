614fc04a406602c0983d5e729e541a3c0a9c7236
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-07-11 20:14:49 -0300

    chore: Add vite-svg-loader for SVG file support

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+<script lang="ts" setup>
+import { RouterView } from 'vue-router'
+import MainHeader from '@/components/common/header/MainHeader.vue'
+import MainFooter from '@/components/common/footer/MainFooter.vue'
 </script>
 
 <template>
+  <MainHeader />
   <RouterView />
+  <MainFooter />
 </template>
 
 <style scoped></style>
