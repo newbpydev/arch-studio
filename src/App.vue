d3bfb89a013bd57fb5189fd271cf359facd27ab0
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-07-11 13:25:29 -0300

    chore: Add initial project files and configurations

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <h1>in app</h1>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
