a34fe50dbf6755625034a8298703f63578d2087a
Juan Gomez <94386361+newbpydev@users.noreply.github.com>
2024-07-11 21:38:42 -0300

    chore: Refactor header component and add mobile responsiveness

--- src/components/common/header/MainHeader.vue
+++ src/components/common/header/MainHeader.vue
@@ -2,13 +2,36 @@
 <script lang="ts" setup>
 import TheHeaderLogo from '@/components/common/icons/TheHeaderLogo.vue'
 import MainNavigation from '@/components/common/navigation/MainNavigation.vue'
+import { onMounted, onBeforeUnmount, ref, watch, onUpdated } from 'vue'
+
+const vWindowSize = {
+  mounted: (el: HTMLElement) => {
+    el.style.color = 'yellow'
+  },
+  updated: (el: any, binding: any, vnode: any) => {
+    console.log('updated', el, binding, vnode)
+  }
+}
+
+const isMobile = ref(false)
+
+const handleResize = () => {
+  isMobile.value = window.innerWidth <= 768
+}
+
+onMounted(() => {
+  window.addEventListener('resize', handleResize)
+})
+onBeforeUnmount(() => {
+  window.removeEventListener('resize', handleResize)
+})
 </script>
 
 /* -------------------------------- Template -------------------------------- */
 <template>
   <header class="container mx-auto">
     <TheHeaderLogo height="10" />
-    <MainNavigation />
+    <MainNavigation v-if="isMobile" />
   </header>
 </template>
 
